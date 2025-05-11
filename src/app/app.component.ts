ea34a6f16f9258fdb914286d9a11a42fa14e01eb
Petar Petrov <petizad55@gmail.com>
2025-05-11 18:11:53 +0200

    fix providers and add environment file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Observable } from 'rxjs';
 import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [AppService]
 })
 export class AppComponent implements OnInit {
   public version: string = 'v0.0.0';
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from "@angular/core";
 import { Observable } from 'rxjs'
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class AppService {
     private readonly http: HttpClient = inject(HttpClient)
-    private readonly API_URL = process.env['API_URL']
+    private readonly API_URL = environment.API_URL
 
     public getVersion(): Observable<string>  {
         return this.http.get<string>(`${this.API_URL}/version`)
