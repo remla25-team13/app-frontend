9075524de4a155361f9fd642d6430ce557c3ba85
rstular <rok@stular.eu>
2025-06-23 11:45:54 +0200

    fix(ui): Send requests when query button is pressed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,14 @@ import { PromptFormComponent } from './prompt-form/prompt-form.component';
 })
 export class AppComponent implements OnInit {
   public version: string = 'v0.0.0';
-  
-  constructor(private readonly appService: AppService) {}
+  public model_type: string = 'unknown';
+
+  constructor(private readonly appService: AppService) { }
 
   ngOnInit(): void {
     this.appService.getVersion().subscribe(response => {
-      this.version = response.version
+      this.version = response.version;
+      this.model_type = response.model_type;
     })
   }
 }
